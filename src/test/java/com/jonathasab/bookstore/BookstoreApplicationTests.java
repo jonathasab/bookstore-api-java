40b2d347101cd1ce9207cbbe4f76e78bb38d8ab7
jonathasab <jonathasab@yahoo.com.br>
2022-02-07 17:12:28 -0300

    commit inicial

---
+++ src/test/java/com/jonathasab/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathasab.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
